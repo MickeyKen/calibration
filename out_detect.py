e23a33109be4f5ab43cb8ec598dcf31bcb3902eb
MickeyKen <mickey333ff@gmail.com>
2019-02-25 03:24:37 +0900

    [Add] UD'kinect2 inner parameter folder

--- out_detect.py
+++ out_detect.py
@@ -7,11 +7,11 @@ corner_num = (col, row)
 size = 2.5
 
 pW = np.empty([row * col, 3], dtype=np.float32)
-K = np.array([ 1.0464088296606685e+03, 0., 9.6962285013582118e+02, 0.,
-       1.0473601981442353e+03, 5.3418043955010319e+02, 0., 0., 1. ],np.float32)
-d = np.array([ 4.3977277514639868e-02, -6.2933078892199332e-02,
+K = np.array([[ 1.0464088296606685e+03, 0., 9.6962285013582118e+02], [0.,
+       1.0473601981442353e+03, 5.3418043955010319e+02], [0., 0., 1. ]],np.float32)
+d = np.array([[ 4.3977277514639868e-02, -6.2933078892199332e-02,
        -5.7377837329916246e-04, 7.8218303056817190e-04,
-       1.4687866930870116e-02 ],np.float32)
+       1.4687866930870116e-02 ]],np.float32)
 
 for i_row in range(0, row):
   for i_col in range(0, col):
@@ -22,7 +22,8 @@ found, qI = cv2.findChessboardCorners(ex_image, corner_num)
 
 if found:
   term = (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_COUNT, 30, 0.1)
-  qI_sub = cv2.cornerSubPix(ex_image, qI, (5,5), (-1,-1), term)
+  qI_sub = cv2.cornerSubPix(ex_image, qI, (9,9), (-1,-1), term)
 
-  ret, rvec, tvec = cv2.solvePnP(qW, qI, K, d)
+  ret, rvec, tvec = cv2.solvePnP(pW, qI, K, d)
   rmat = cv2.Rodrigues(rvec)[0]
+  print rmat
