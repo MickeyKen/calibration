e7c9944642b1359b5f4251fe4bbc9c7a6bc4daee
MickeyKen <mickey333ff@gmail.com>
2019-02-25 02:59:44 +0900

    [Add] K and d

--- out_detect.py
+++ out_detect.py
@@ -7,6 +7,11 @@ corner_num = (col, row)
 size = 2.5
 
 pW = np.empty([row * col, 3], dtype=np.float32)
+K = np.array([ 1.0464088296606685e+03, 0., 9.6962285013582118e+02, 0.,
+       1.0473601981442353e+03, 5.3418043955010319e+02, 0., 0., 1. ],np.float32)
+d = np.array([ 4.3977277514639868e-02, -6.2933078892199332e-02,
+       -5.7377837329916246e-04, 7.8218303056817190e-04,
+       1.4687866930870116e-02 ],np.float32)
 
 for i_row in range(0, row):
   for i_col in range(0, col):
@@ -19,6 +24,5 @@ if found:
   term = (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_COUNT, 30, 0.1)
   qI_sub = cv2.cornerSubPix(ex_image, qI, (5,5), (-1,-1), term)
 
-  pts_src = np.array([[0.0,0.0], [1919.0,0.0], [1919.0,1079.0],[0.0,1079.0]],np.float32)
   ret, rvec, tvec = cv2.solvePnP(qW, qI, K, d)
   rmat = cv2.Rodrigues(rvec)[0]
